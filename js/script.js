d1b764d26f603af693582185871c8d41fa13d206
Владимир <kitayev.vovochka@bk.ru>
2020-12-16 13:55:14 +0300

    Изменил документ

---
+++ js/script.js
@@ -0,0 +1,54 @@
+function getExpensesMonth(car, gym, utilities) {
+    return car + gym + utilities;
+};
+
+console.log('Сумма всех обязательных расходов на месяц:' + ' ' + getExpensesMonth(5000, 1000, 8000));
+
+
+function getAccumulatedMonth(income, expenses) {
+    return income - expenses;
+};
+
+console.log('Накопления за месяц: ' + ' ' + getAccumulatedMonth(20000, 14000));
+
+
+
+let accumulatedMonth = 6000;
+
+
+function getTargetMonth(day, day1) {
+  return day + day1;
+};
+
+
+console.log('Подсчитывает за сколько дней будет достигнут результат: ' + ' ' + getTargetMonth(30, 30));
+
+
+function showTypeOf() {
+
+};
+
+showTypeOf();
+
+
+
+
+const addExpenses = ['sweets', 'clothes', 'illnes'];
+
+
+
+function getTargetMonth(month, month1) {
+    return month + month1;
+};
+
+
+
+console.log('Срок достижения цели в месяцах: ' +  getTargetMonth(2, 2));
+
+
+
+function getStatusIncome() {
+
+};
+
+getStatusIncome();
