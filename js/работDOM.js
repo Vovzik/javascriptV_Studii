615abed1d0afe609455223741e586a663144cb90
Your Name <you@example.com>
2021-03-01 11:41:42 +0300

    Добавил элементы

--- js/работDOM.js
+++ js/работDOM.js
@@ -13,4 +13,19 @@ console.log(collections[0].before(collections[1]));
 
 elems[1].after(elems[3]);
 
-elems[3].replaceWith('Привет!');
+elems[3].replaceWith('Привет!'); 
+
+elems[4].insertAdjacentText('beforebegin', 'beforebegin'); //Добавляет элемент до заголовка
+elems[4].insertAdjacentText('afterend', 'afterend');
+elems[4].insertAdjacentText('afterbegin', 'afterbegin');
+elems[4].insertAdjacentText('beforeend', 'beforeend');
+
+
+
+elems[5].insertAdjacentElement('beforebegin', elems); //Добавляет элемент до заголовка
+
+
+
+elems[0].insertAdjacentHTML('beforebegin', '<h3>beforebegin</h3>')
+
+
