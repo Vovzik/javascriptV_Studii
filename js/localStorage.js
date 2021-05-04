ee978f2073fd46bb0a94c09f29e56a3f6b6543d5
Your Name <you@example.com>
2021-05-04 17:51:20 +0300

    Изменил документ

--- js/localStorage.js
+++ js/localStorage.js
@@ -34,4 +34,23 @@ document.cookie = 'имя1=значение2';
 
 document.cookie = 'hope=life; expires=Tue, 7 May 2024 00:00:00 GMT';  */
 
-console.log(document.cookie)
+function setCookie(key, value, year, month, day, path, domain, secure) {
+  let cookieStr = key + '=' + value;
+
+  if(year) {
+    const expires = new Date(year, month, day);
+    cookieStr += '; expires=' + expires.toGMTString();
+  };
+
+  cookieStr += path ? '; path=' + path : '';
+  cookieStr += domain ? '; domain=' + domain : '';
+  cookieStr += secure ? '; secure' : '';
+
+  document.cookie = cookieStr;
+}
+
+
+setCookie('Любимый праздник детей', 'Новой год', 2020);
+
+
+/* console.log(document.cookie) */
