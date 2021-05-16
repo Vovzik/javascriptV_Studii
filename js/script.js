65defdc1f1f8e5f4f71819c49597d798b745dd4e
Your Name <you@example.com>
2021-05-16 15:24:45 +0300

    Изменил документ

--- js/script.js
+++ js/script.js
@@ -8,21 +8,65 @@ const todoControl = document.querySelector('.todo-control'),
 
 const todoData = [
   {
-    value: 'Сварить кофе',
+    value: 'Помыть посуду',
     completed: false
   },
 
   {
-    value: 'Помыть посуду',
+    value: 'Сварить кофе',
     completed: true
   }
 ]
 
 const renderer = () => {
+  todoList.textContent = null;
+  todoCompleted.textContent = null;
 
+
+
+  todoData.forEach(function (item) {
+    const li = document.createElement('li');
+    li.classList.add('todo-item');
+
+   
+
+    li.innerHTML = '<span class="text-todo"> ' + item.value + '</span>' +
+      '<div class="todo-buttons">' +
+      '<button class="todo-remove"></button>' +
+      '<button class="todo-complete"></button>' +
+      '</div>'
+
+   
+
+    if (item.completed) {
+      todoCompleted.append(li);
+    } else {
+      todoList.append(li);
+    }
+
+
+    const btn = li.querySelector('.todo-complete');
+    
+    btn.addEventListener('click', () => {
+         console.log(item.completed = !item.completed);
+         renderer(); //Рекурсия это когда надо по новой вызвать функцию с начало вызвать.
+    });
+  });
 }
 
 
 todoControl.addEventListener('submit', (event) => {
   event.preventDefault(todoControl);
+
+
+  const newTodo = {
+    value: headerInput.value,
+    completed: false
+  }
+
+  todoData.push(newTodo);
+
+  renderer();
 })
+
+renderer();
